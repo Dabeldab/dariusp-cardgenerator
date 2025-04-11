d805174010d0c9b895fa5d6154c8d939a58a024a
Dabeldab <1dareios@gmail.com>
2025-04-11 21:25:41 +0000

    complete with a functioning button

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
 import "bootstrap";
 import "./style.css";
+import { Button } from "bootstrap";
+//const dataElement = document.querySelector(cardnumber);
+const cardnumber = [1,2,3,4,5,6,7,8,9,10,'A','J','Q','K']
+const faces = ['♦', '♥', '♠', '♣']
 
 
+window.onload = function () {
+  shuffle()
+}
 
+function shuffle() {
+  const randomCardIndex = Math.floor(Math.random() * cardnumber.length)
+  const randomFaceIndex = Math.floor(Math.random() * faces.length)
 
-window.onload = function() {
-  //write your code here
-const values = [1,2,3,4,5,6,7,8,9,10,A,J,Q,K]
-const faces = ['♦','♥','♠','♣']
+  const selectedCard = cardnumber[randomCardIndex]
+  const selectedFace = faces[randomFaceIndex]
 
+  const numberCard = document.getElementById("cardnumber")
+  numberCard.innerHTML = `${selectedCard}`
+  
+  const suitTop = document.querySelector(".suit-top p")
+  suitTop.innerHTML = `${selectedFace}`
+
+  const suitBottom = document.querySelector(".suit-bottom p")
+  suitBottom.innerHTML = `${selectedFace}`
+
+  if (selectedFace === faces[0] || selectedFace === faces[1]) {(suitTop.style.color = 'red', suitBottom.style.color   = 'red', numberCard.style.color = 'red')}
+  else suitTop.style.color = 'black',suitBottom.style.color   = 'black', numberCard.style.color = 'black';
 };
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const push = document.getElementById("refreshbutton");
+  if (push) { push.addEventListener("click", shuffle)    
+  }
+})
